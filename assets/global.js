2273b0c78629ff19ba93d9ea12e82b3009aa9725
ETBlue <etblue.tw@gmail.com>
2016-11-11 22:15:23 +0800

    deploy

--- assets/global.js
+++ assets/global.js
@@ -1,7 +1,6 @@
-$(document).ready(function(){
-
+$(function(){
   $('.ui.sidebar').sidebar('attach events', '[data-toggle="sidebar"]');
-
   $('.ui.dropdown').dropdown();
-
+  $('.ui.sticky').sticky();
+  $('.ui.checkbox').checkbox();
 });
