92e7c83fe9c4f67ea01e4b711496f83da11b3ab8
ETBlue <etblue.tw@gmail.com>
2016-11-22 15:45:16 +0800

    deploy

---
+++ assets/redirect.js
@@ -0,0 +1,16 @@
+$(function(){
+
+$("[data-url]").each(function(){
+  var url = $(this).attr("data-url");
+  $.get(url, function( html ){
+    var $page = $(html);
+    var title = $page.filter("title").text();
+    var target = $page.filter("meta[http-equiv='refresh']").attr("content");
+    console.log( target.indexOf("url=") + 4 );
+    target = target.slice( target.indexOf("url=") + 4 );
+    $("[data-url='"+url+"']").find("[data-value='title']").text(title);
+    $("[data-url='"+url+"']").find("[data-value='redirect url'] pre").text(target);
+  });
+});
+
+});
