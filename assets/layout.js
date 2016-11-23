16f148ba7ed5a66ce85d6d2faddcca2c2e4f664e
ETBlue <etblue.tw@gmail.com>
2016-11-23 12:54:59 +0800

    deploy

--- assets/layout.js
+++ assets/layout.js
@@ -5,12 +5,7 @@ $("[data-url]").each(function(){
   $.get(url, function( html ){
     var $page = $(html);
     var title = $page.filter("title").text();
-    var target = $page.filter("meta[http-equiv='refresh']").attr("content");
-    if (target) {
-    target = target.slice( target.indexOf("url=") + 4 );
-    }
     $("[data-url='"+url+"']").find("[data-value='title']").text(title);
-    $("[data-url='"+url+"']").find("[data-value='redirect url'] pre").text(target);
   });
 });
 
