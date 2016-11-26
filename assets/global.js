25452e4bf44e97e714ad8f60523641b8ae043091
ETBlue <etblue.tw@gmail.com>
2016-11-26 09:12:20 +0800

    clear repo

--- assets/global.js
+++
@@ -1,6 +0,0 @@
-$(function(){
-  $('.ui.sidebar').sidebar('attach events', '[data-toggle="sidebar"]');
-  $('.ui.dropdown').dropdown();
-  $('.ui.sticky').sticky();
-  $('.ui.checkbox').checkbox();
-});
