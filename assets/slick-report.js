3dff6e6cd2a83e57c6a79763e0358cd97d4ae8bf
ETBlue <etblue.tw@gmail.com>
2017-02-07 15:21:16 +0800

    add image slider

---
+++ assets/slick-report.js
@@ -0,0 +1,13 @@
+$(function(){
+
+  $('.slick').slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    fade: false,
+    dots: false,
+    infinite: true,
+    speed: 500,
+    cssEase: 'linear'
+  });
+
+});
