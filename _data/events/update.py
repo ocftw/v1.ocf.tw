9e7c0d9ea9813450d38b5a6803aa3a3ce31794a9
Pellaeon Lin <nfsmwlin@gmail.com>
2019-03-25 12:46:40 +0100

    Update event script

--- _data/events/update.py
+++ _data/events/update.py
@@ -12,18 +12,19 @@ respE = codecs.iterdecode(resp, 'utf-8')
 reader = csv.DictReader(respE)
 for row in reader:
     #print(row['event_id'])
-    fstring = """{event_id}:
+    fstring1 = """{event_id}:
   title: "{title}"
   description: "{description}"
   month: "{month}"
   start: {start}
   end: {end}
-  link:
+  link:"""
+    fstring2 = """
     - title: {link_tl1}
-      url: {link1}
-    - title: {link_tl2}
-      url: {link2}
-  cover: {cover}
+      url: {link1}""" if row['link1'] else None
+    fstring3 = """    - title: {link_tl2}
+      url: {link2}""" if row['link2'] else None
+    fstring4 = """  cover: {cover}
   contributor_id: {contributor_id}
   project_id:
     - {project_id}
@@ -38,4 +39,7 @@ for row in reader:
   speaker_id: {speaker_id}
   banner: {banner}
   avatar: {avatar}"""
-    print(fstring.format(**row))
+    print(fstring1.format(**row))
+    if fstring2: print(fstring2.format(**row))
+    if fstring3: print(fstring3.format(**row))
+    print(fstring4.format(**row))
