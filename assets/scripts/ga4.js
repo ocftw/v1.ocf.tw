89bec75f7465c1ff7054808826e4ff56bd580e20
leaf0922 <jing838383@gmail.com>
2023-04-25 10:40:12 +0800

    Update ga4.js

--- assets/scripts/ga4.js
+++ assets/scripts/ga4.js
@@ -1,8 +1,10 @@
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-X3DG2E5E3W"> 
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-X3DG2E5E3W"></script>
 
+<script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
   gtag('config', 'G-X3DG2E5E3W');
+  gtag('event', 'page_view', { 'send_to': 'G-X3DG2E5E3W' });
+</script>
