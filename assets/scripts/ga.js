e37c450ca0ec80d2c252be82a364260cb831b26e
leaf0922 <jing838383@gmail.com>
2023-05-08 13:19:29 +0800

    Update ga.js

--- assets/scripts/ga.js
+++ assets/scripts/ga.js
@@ -10,3 +10,12 @@ ga('require', 'linker');
 ga('linker:autoLink', ['neticrm.tw', 'kktix.cc', 'kktix.com']);
 
 ga('send', 'pageview');    
+
+
+<script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "h0k16cetd0");
+</script>
